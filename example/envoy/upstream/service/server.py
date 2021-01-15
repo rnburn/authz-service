3e8cc5ebc834492c2ba43cdd2662df5ef39d257f
Ryan Burn <ryan.burn@gmail.com>
2021-01-14 19:25:00 -0800

    Work on envoy example

---
+++ example/envoy/upstream/service/server.py
@@ -0,0 +1,39 @@
+from flask import Flask, request
+import sys
+from opentelemetry import trace
+from opentelemetry.exporter import zipkin
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry import propagators
+from opentelemetry.sdk.trace.propagation.b3_format import B3Format
+
+propagators.set_global_textmap(B3Format())
+
+trace.set_tracer_provider(TracerProvider())
+tracer = trace.get_tracer(__name__)
+
+# create a ZipkinSpanExporter
+zipkin_exporter = zipkin.ZipkinSpanExporter(
+    service_name="my-helloworld-service",
+    url = "http://zipkin:9411/api/v2/spans",
+)
+span_processor = BatchExportSpanProcessor(zipkin_exporter)
+trace.get_tracer_provider().add_span_processor(span_processor)
+
+app = Flask(__name__)
+FlaskInstrumentor().instrument_app(app)
+
+
+@app.route('/service')
+def hello():
+  sys.stderr.write(str(request.headers))
+  print(request.headers)
+  return 'Hello from behind Envoy!'
+
+
+if __name__ == "__main__":
+  # zipkin = Zipkin(app, sample_rate=100)
+  # app.config['ZIPKIN_DISABLE'] = False
+  # app.config['ZIPKIN_DSN'] = "http://zipkin:9411/api/v1/spans"
+  app.run(host='0.0.0.0', port=8080, debug=False)
